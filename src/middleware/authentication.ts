fcc2310f0a1664d93bd4386a1a60e7007b7b0cd4
ShroukMagdy3 <shorokmagdy15@gmail.com>
2025-11-15 01:34:59 +0200

    refactor

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -12,8 +12,8 @@ export enum tokenEnum {
 
 export const Authentication = (tokenType: tokenEnum) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const { Authorization } = req.headers;
-    const [prefix, token] = Authorization?.split(" ") || [];
+    const { authorization } = req.headers;
+    const [prefix, token] = authorization?.split(" ") || [] ;
     if (!prefix || !token) {
       throw new AppError("invalid token", 401);
     }
